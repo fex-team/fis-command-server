dc5a89a0048d86df7a4357fa474ee961fc2a0299
zhangyunlong <zhangyunlong@baidu.com>
2013-04-26 01:07:17 +0800

    美化一下默认的index.php

--- index.php
+++ index.php
@@ -4,17 +4,13 @@
 <meta charset="utf-8"/>
 <title>hello, fis</title>
 <style>
-    hr {
-        padding: 0;
-        margin: 0;
-    }
     pre {
         line-height: 7px;
         font-family: "Courier New", "Courier", "mono";
         font-size: 11px;
-        color: #333
-        padding: 0;
-        margin: 0;
+        color: #333;
+        border-bottom: 1px dashed #999;
+        padding-bottom: 1px;
     }
     .c1 {
         color: #c90;
@@ -25,60 +21,58 @@
 </style>
 </head>
 <body>
-
 <pre>
-                                                            
-                                                            
-                                                            
-                           &lt;+    .s'                        
-                   .'     (B+     DD      '                 
-           .       'D'    hD'     sB~    sh                 
-           (h'     'BD    -        ''   -Bh      .-         
-           'BD.''   zh.- .            ''-D'     'Ds         
-         .&lt;~zD-~&lt;  ..''(..   .   . ...-~'' . -- hB-         
-        sBBBh-''. .'. .. '      .   .-... ...~-.hz&lt;h~       
-        &lt;hDD&lt; ... ...  ''. .=-   +-. .'.  ..  .''zBBBh      
-             -D=.'..  '.- . '&lt;  '~ .  '..  ...'' .zhz-      
-             DD-........'  ...   ...  ..  ..' &lt;Bs           
-            .~  '=.  .. .   .           ..  -. zD'          
-                 . -. ...  zh          .. ..('  ..          
-            ~=     zD( .' =Bh        '.  =B.     =-         
-            hD.    ~BD..'.DB~        -.''Ds.... .Dh         
-            DB~   . =h'~--=~'     h'.. '&lt;=&lt;(-'' ~BD         
-            hB' .hBh.+&lt;..''Ds    'B+' .=~.      -Bh         
-            ~s   =z&lt; .(s..=B++-  .h( ~=~         z~         
-          ~z-  ((   ~h==h.-('BB(-&lt;-'-+(BB(    (( .-z(       
-          'DD'DB(.. =h+'sD'-.&lt;BD~''-s .&lt;+.  ..~BD'hD'       
-             .~~.  . '.('zD''.~-'.'zhh. .' . ..~~. .        
-               . .s'~B~.. hz..Dh.'=DD+  (D.'s. .            
-          (.  ' &lt;z''zBs   'B( ~D(-D('   sB=.'z+ '  .(       
-          sB' . hD..zBh- . =D..''s='&lt;. -hBh..hh . .Bs       
-           h( .'~~. &lt;DDh   .D&lt;.''h'.'  DzDs  -~'. ~h        
-            '+.-. .'~~DB... =D.'-z   ..Bh'~'. .'.+'         
-         .. sB''(..(&lt;'hz.D- 'B' ++  -D.hh'+(.'(.'Dz ..      
-         ...=h..hD'. .-''D-  Ds h~  ~D'''. .'Ds  h= ..      
-         . ...  'D~-~''-.~ . +D.h.  .(.''.'.'z.  ... .      
-              ~-'~(-~~-s-. . ~D-h    .&lt;z(-''~. ..           
-        ((  '~...'.    .~=~  .Dhh  .=Ds'    .-.  .   &lt;(     
-        (B&lt;''  .'. .     .zs. DBh 'DD-     . .'.  . =B-     
-         .+.  .'D- .       zD.hBz.DD.        -D'    (.      
-          =' ''(B~          hhhBzhB~         ~B(-. ~~       
-         -zD.hD~h           .BBBDBh           h(Dz.B=       
-        sh.h~BB-             zBBBB(            (BD&lt;z'D&lt;     
-       .BD  'BD              ~BBBB'            .DB' -BB.    
-       .BD   D(              -BBBD.             &lt;h  'BB'    
-        Ds   '               'BBBD.              .   zD     
-        '                    -BBBD.                   (     
-                             -BBBB.                         
-                             (BBBB-                         
-                             =BBBB=                         
-                             DBBBBh          
-                            'BBBBBB'                        
-                            sBBBBBB=     Rewrite request [ <em class="c1"><?php echo $_SERVER['REQUEST_URI'];?></em> ] into file [ <em class="c2"><?php echo __FILE__;?></em> ].          
-                           .DBBBBBBD                        
-                           +BBBBBBBB+             
+                                                       
+                                                       
+                                                       
+                      &lt;+    .s'                        
+              .'     (B+     DD      '                 
+      .       'D'    hD'     sB~    sh                 
+      (h'     'BD    -        ''   -Bh      .-         
+      'BD.''   zh.- .            ''-D'     'Ds         
+    .&lt;~zD-~&lt;  ..''(..   .   . ...-~'' . -- hB-         
+   sBBBh-''. .'. .. '      .   .-... ...~-.hz&lt;h~       
+   &lt;hDD&lt; ... ...  ''. .=-   +-. .'.  ..  .''zBBBh      
+        -D=.'..  '.- . '&lt;  '~ .  '..  ...'' .zhz-      
+        DD-........'  ...   ...  ..  ..' &lt;Bs           
+       .~  '=.  .. .   .           ..  -. zD'          
+            . -. ...  zh          .. ..('  ..          
+       ~=     zD( .' =Bh        '.  =B.     =-         
+       hD.    ~BD..'.DB~        -.''Ds.... .Dh         
+       DB~   . =h'~--=~'     h'.. '&lt;=&lt;(-'' ~BD         
+       hB' .hBh.+&lt;..''Ds    'B+' .=~.      -Bh         
+       ~s   =z&lt; .(s..=B++-  .h( ~=~         z~         
+     ~z-  ((   ~h==h.-('BB(-&lt;-'-+(BB(    (( .-z(       
+     'DD'DB(.. =h+'sD'-.&lt;BD~''-s .&lt;+.  ..~BD'hD'       
+        .~~.  . '.('zD''.~-'.'zhh. .' . ..~~. .        
+          . .s'~B~.. hz..Dh.'=DD+  (D.'s. .            
+     (.  ' &lt;z''zBs   'B( ~D(-D('   sB=.'z+ '  .(       
+     sB' . hD..zBh- . =D..''s='&lt;. -hBh..hh . .Bs       
+      h( .'~~. &lt;DDh   .D&lt;.''h'.'  DzDs  -~'. ~h        
+       '+.-. .'~~DB... =D.'-z   ..Bh'~'. .'.+'         
+    .. sB''(..(&lt;'hz.D- 'B' ++  -D.hh'+(.'(.'Dz ..      
+    ...=h..hD'. .-''D-  Ds h~  ~D'''. .'Ds  h= ..      
+    . ...  'D~-~''-.~ . +D.h.  .(.''.'.'z.  ... .      
+         ~-'~(-~~-s-. . ~D-h    .&lt;z(-''~. ..           
+   ((  '~...'.    .~=~  .Dhh  .=Ds'    .-.  .   &lt;(     
+   (B&lt;''  .'. .     .zs. DBh 'DD-     . .'.  . =B-     
+    .+.  .'D- .       zD.hBz.DD.        -D'    (.      
+     =' ''(B~          hhhBzhB~         ~B(-. ~~       
+    -zD.hD~h           .BBBDBh           h(Dz.B=       
+   sh.h~BB-             zBBBB(            (BD&lt;z'D&lt;     
+  .BD  'BD              ~BBBB'            .DB' -BB.    
+  .BD   D(              -BBBD.             &lt;h  'BB'    
+   Ds   '               'BBBD.              .   zD     
+   '                    -BBBD.                   (     
+                        -BBBB.                         
+                        (BBBB-                         
+                        =BBBB=                         
+                        DBBBBh          Forward request [ <em class="c1"><?php echo $_SERVER['REQUEST_URI'];?></em> ] to file [ <em class="c2"><?php echo __FILE__;?></em> ].
+                       'BBBBBB'                        
+                       sBBBBBB=               
+                      .DBBBBBBD                        
+                      +BBBBBBBB+             
 </pre>
-<hr/>
 
 </body>
 </html>
